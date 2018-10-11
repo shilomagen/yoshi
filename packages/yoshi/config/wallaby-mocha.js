2a08acc8b1a417dce548a4dca2b22ff2be4ce3de
Artem Yavorsky <aqson@me.com>
2018-10-11 14:06:01 +0300

    Babel 7: Update wallaby config

--- packages/yoshi/config/wallaby-mocha.js
+++ packages/yoshi/config/wallaby-mocha.js
@@ -1,5 +1,16 @@
 module.exports = function(wallaby) {
   const wallabyCommon = require('./wallaby-common')(wallaby);
+  wallabyCommon.tests.unshift(
+    { pattern: 'test/**/*.+(spec|it).[j|t]s' },
+    { pattern: 'test/**/*.+(spec|it).[j|t]sx' },
+  );
+  wallabyCommon.compilers = {
+    '**/*.js{,x}': wallaby.compilers.babel({
+      babel: require('@babel/core'),
+      babelrc: true,
+      plugins: [require.resolve('@babel/plugin-transform-modules-commonjs')],
+    }),
+  };
   wallabyCommon.testFramework = 'mocha';
   wallabyCommon.setup = () => {
     const mocha = wallaby.testFramework;
