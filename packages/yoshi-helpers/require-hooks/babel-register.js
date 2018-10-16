a3927049ed1217c7c735fca29583157c2b8750f5
Artem Yavorsky <aqson@me.com>
2018-10-16 18:07:44 +0300

    Babel 7: [yoshi-helpers] Fix @babel/core dependency for tests.

--- packages/yoshi-helpers/require-hooks/babel-register.js
+++ packages/yoshi-helpers/require-hooks/babel-register.js
@@ -1,6 +1,6 @@
 const { unprocessedModules } = require('yoshi-config');
 
 require('@babel/register')({
-  only: unprocessedModules,
+  only: [unprocessedModules],
   plugins: [require.resolve('@babel/plugin-transform-modules-commonjs')],
 });
